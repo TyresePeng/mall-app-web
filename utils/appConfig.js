66f2fbf34617c2598daf9177593099891e66c196
peng.guo <peng.guo@loreal.com>
2024-11-12 18:35:42 +0800

    微信登录

--- utils/appConfig.js
+++ utils/appConfig.js
@@ -1,3 +1,7 @@
+export const CONTENT_TYPE_JSON = "application/json;charset=UTF-8";
+export const CONTENT_TYPE_urlencoded = 'application/x-www-form-urlencoded;charset=utf-8';
+
+
 // appConfig.js
 
 //配置API请求的基础路径
